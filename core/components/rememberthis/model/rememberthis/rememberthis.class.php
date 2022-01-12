7277bfb16bcdd10e588422bd837a01398009745d
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-12 15:19:49 +0100

    Normalize code

--- core/components/rememberthis/model/rememberthis/rememberthis.class.php
+++ core/components/rememberthis/model/rememberthis/rememberthis.class.php
@@ -58,7 +58,7 @@ class RememberThis
      */
     public function __construct(modX &$modx, array $options = array())
     {
-        $this->modx = &$modx;
+        $this->modx =& $modx;
 
         $this->modx->lexicon->load('rememberthis:default');
 
